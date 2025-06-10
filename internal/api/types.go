f3a689a0ce148b5c08f4de86e01b48c87ff15e93
harachan <44335168+harakeishi@users.noreply.github.com>
2025-06-10 14:41:27 +0900

    Update README and entry script

--- internal/api/types.go
+++ internal/api/types.go
@@ -3,18 +3,17 @@ package api
 // Discussion summary information
 
 type Discussion struct {
-    Title    string
-    URL      string
-    Author   string
-    Comments int
+	Title    string
+	URL      string
+	Author   string
+	Comments int
 }
 
 // DiscussionDetail holds detail of a discussion
 
 type DiscussionDetail struct {
-    Title  string
-    Author string
-    Body   string
-    URL    string
+	Title  string
+	Author string
+	Body   string
+	URL    string
 }
-
